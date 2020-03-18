48a05bb4a97a016f8bdd2800f7233203d383ada1
Joshua_Park <nirvana0081@gmail.com>
2020-03-18 21:08:22 +0900

    update

--- insta_1.py
+++
@@ -1,7 +0,0 @@
-import selenium
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-import warnings
-warnings.filterwarnings(action='ignore')
