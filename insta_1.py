d7b069ec560317d84272a550bc41c5e4895c1284
Joshua_Park <nirvana0081@gmail.com>
2020-03-18 21:03:54 +0900

    Initial commit

---
+++ insta_1.py
@@ -0,0 +1,7 @@
+import selenium
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import warnings
+warnings.filterwarnings(action='ignore')
